0f78aca99057d54b1050bd151f33edd47726ef1f
Vsevolod Makhinya <Vsevolod Makhinya@filmon.com>
2014-12-10 12:17:22 +0200

    Filling main window with black color

--- src/info/dvad/pabase/Main.java
+++ src/info/dvad/pabase/Main.java
@@ -1,19 +1,20 @@
 package info.dvad.pabase;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500, Color.BLACK);
+        stage.setTitle("JavaFX Scene Graph Demo");
+        stage.setScene(scene);
+        stage.show();
     }
     public static void main(String[] args) {
         launch(args);
